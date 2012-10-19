add6f79172bbf06b65e22f7add6e6870ebaa8a5d
nileader <nileader@gmail.com>
2012-10-19 10:39:53 +0800

    DateUtil support solr format

--- src/main/java/common/toolkit/java/entity/DateFormat.java
+++ src/main/java/common/toolkit/java/entity/DateFormat.java
@@ -6,7 +6,7 @@ package common.toolkit.java.entity;
  * @Date	 Feb 15, 2012
  */
 public enum DateFormat {
-	Date("yyyy-MM-dd"), DateTime("yyyy-MM-dd HH:mm:ss");
+	Date("yyyy-MM-dd"), DateTime("yyyy-MM-dd HH:mm:ss"), SolrDateTime("yyyy-MM-ddTHH:mm:ssZ") ;
 
 	private final String format;
 	private DateFormat( String format ) {
